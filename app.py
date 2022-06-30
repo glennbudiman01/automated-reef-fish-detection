df6e423c10aea83034f795d7cadbc730efcad156
glennbudiman01 <glennbudiman01@gmail.com>
2022-06-30 12:56:06 +0800

    add modal and add predict page

--- app.py
+++ app.py
@@ -40,9 +40,22 @@ def get_output():
         X = reshape([X])
         y = model.predict(X)
 
-        if labels[np.argmax(y)] == 'Swanggi':
+        if labels[np.argmax(y)] == 'Kakap':
+            gambar = './static/img/gambar/kakap1.jpg'
+            penjelasan = 'Kakap adalah keluarga ikan laut dasaran (demersal) yang hidup secara berkelompok di dasar-dasar karang atau terumbu karang. Kakap termasuk famili Lutjanidae. Mempunyai ciri tubuh yang bulat pipih dengan sirip memanjang sepanjang punggung. Jenis ikan kakap yang banyak ditemui di Indonesia adalah jenis kakap merah (L. campechanus). Beberapa jenis yang lain yang juga banyak ditemui adalah kakap kuning, kakap hitam dan lain-lain.'
+
+        elif labels[np.argmax(y)] == 'Pogot':
+            gambar = './static/img/gambar/pogot1.jpg'
+            penjelasan = 'Ikan Pogot atau Triggerfish yang bernama ilmiah Aluterus hidupnya di perairan laut dalam yang berkarang. Bentuknya pipih seperti ikan bawal serta memiliki gigi yang besar dan tajam serta berkulit tebal sangat. Walaupun ciri fisiknya menyeramkan ternyata ikan ini cukup enak untuk dikonsumsi, menurut orang-orang yang pernah memakan ikan ini rasanya seperti ayam, sebab itu di sebagian daerah Indonesia menyebutnya ikan ayam-ayam. Ikan ini tergolong kedalam jenis ikan omnivora yaitu ikan pemakan segala.'
+
+        elif labels[np.argmax(y)] == 'Rengginan':
+            gambar = './static/img/gambar/swanggi1.jpeg'
+            penjelasan = ''
+
+        else:
             gambar = './static/img/gambar/swanggi1.jpeg'
             penjelasan = 'Ikan swanggi (Priacanthus tayenus Richardson, 1846) termasuk salah satu jenis dari ikan demersal yang umumnya mencari makan secara nokturnal dan diurnal dengan sama baiknya (Starnes 1984). Ikan ini juga bernilai ekonomis dan ekologis tetapi masih belum dikaji.'
+
         return render_template("predict.html", result=labels[np.argmax(y)], gambar=gambar, penjelasan=penjelasan)
 
 
